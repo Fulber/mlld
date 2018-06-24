18db1a22a33a4cbcabe6c7c78bcbb1594fa16a19
Unknown <ovidiu.negoita95@gmail.com>
2018-06-24 23:28:21 +0300

    F1.0

--- mlld/regr_trainer.py
+++ mlld/regr_trainer.py
@@ -1,21 +1,22 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold, train_test_split, GridSearchCV
-from sklearn.metrics import classification_report, precision_score, make_scorer
+from sklearn.metrics import classification_report, precision_score, f1_score, make_scorer
 from utils.data_reader import DataReader as dr
 import sys, getopt, numpy as np
 
 class Regression(object):
 
-	def __init__(self, decimal_precision, debug, optimise, exp):
+	def __init__(self, decimal_precision, debug, optimize, exp):
 		np.set_printoptions(threshold = np.inf)
 		self.precision = decimal_precision
 		self.debug = debug
 		self.regr = LogisticRegression(class_weight = 'balanced')
-		if optimise:
+		if optimize:
 			if exp:
-				self.regr = LogisticRegression(C = 1000, tol = 1e-3, class_weight = 'balanced')#experimental optimised
+				self.regr = LogisticRegression(C = 1000, tol = 1e-3, class_weight = 'balanced')#experimental optimized
 			else:
-				self.regr = LogisticRegression(class_weight = 'balanced')
+				# precision and f1_score optimized
+				self.regr = LogisticRegression(C = 100,  tol = 1e-3, class_weight = 'balanced')
 		
 	def train(self, features, labels):
 		non_zero = np.count_nonzero(labels != 0)
@@ -29,17 +30,12 @@ class Regression(object):
 		labels = data[:, -1].astype(int)
 		
 		print('[INFO] ', np.count_nonzero(labels != 0), 'links present in training data, out of ', len(labels))
-
 		k_fold = KFold(n_splits = 2)
 		for train, test in k_fold.split(features):
-			print('[INFO] Round of testing')
 			self.train(features[train], labels[train])
-			
 			preds = self.regr.predict(features[test])
 			print('[INFO] ', np.count_nonzero(preds != 0), ' links retrieved by model')
-			
-			succ_pred = np.count_nonzero(preds == labels[test])
-			print('[INFO] Succesfully predicted ', succ_pred, ' links out of ', len(labels[test]))
+			print('[INFO] Succesfully predicted ', np.count_nonzero(preds == labels[test]), ' links out of ', len(labels[test]))
 
 	def validate(self, file):
 		data = dr(file).read_data(precision = self.precision)
@@ -51,11 +47,8 @@ class Regression(object):
 		for train, test in k_fold.split(features):
 			self.train(features[train], labels[train])
 			preds = self.regr.predict(features[test])
-			precision = precision_score(labels[test], preds, pos_label = 1, average = 'binary')
-			if self.debug:
-				print('[INFO] ', np.count_nonzero(preds != 0), ' links retrieved by model')
-				print(classification_report(labels[test], preds, target_names = ['class 0', 'class 1']))
-			result.append(precision)
+			result.append(precision_score(labels[test], preds, pos_label = 1, average = 'binary'))
+			dr.print_report(self.debug, preds, labels[test]) #Print Classification Report
 		return result
 
 	def validate_proba(self, file, proba_tol):
@@ -67,31 +60,9 @@ class Regression(object):
 		k_fold = KFold(n_splits = 2)
 		for train, test in k_fold.split(features):
 			self.train(features[train], labels[train])
-			preds = []
-			feature = [features[test[0]]]
-			for x in test[1:]:
-				if features[x - 1][-2] > features[x][-2]:
-					preds.extend(self.predict_from_proba(self.regr.predict_proba(feature), proba_tol))
-					feature = [features[x]]
-				else:
-					feature.append(features[x])
-			preds.extend(self.predict_from_proba(self.regr.predict_proba(feature), proba_tol))
-			if self.debug:
-				print('[INFO] ', np.count_nonzero(preds != 0), ' links retrieved by model')
-				print(classification_report(labels[test], preds, target_names = ['class 0', 'class 1']))
+			preds = self.proba_util(features[test], proba_tol)
 			result.append(precision_score(labels[test], preds, pos_label = 1))
-		return result
-
-	def predict_from_proba(self, proba, proba_tol):
-		max = 0
-		for i, val in enumerate(proba):
-			if val[1] > max and val[1] >= proba_tol:
-				max = val[1]
-				idx = i
-
-		result = np.zeros(len(proba), dtype = int)
-		if max != 0:
-			result[idx] = 1
+			dr.print_report(self.debug, preds, labels[test]) #Print Classification Report
 		return result
 
 	def tune_parameters(self, file):
@@ -100,19 +71,65 @@ class Regression(object):
 		labels = data[:, -1].astype(int)
 		
 		fT, ft, lT, lt = train_test_split(features, labels, test_size = 0.5, random_state = 0)
-		parameters = [{'C': [1, 10, 100, 1000], 'tol': [1e-3, 1e-4, 1e-5]}]
+		parameters = [{'C': [1, 5, 10, 50, 100, 500, 1000], 'tol': [1e-2, 1e-3, 1e-4, 1e-5, 1e-6], 
+			'class_weight': ['balanced', None]}]
 		
-		clf = GridSearchCV(LogisticRegression(class_weight = 'balanced'), parameters, cv = 2, scoring = make_scorer(precision_score, pos_label = 1))
+		clf = GridSearchCV(LogisticRegression(), parameters, cv = 2, scoring = make_scorer(precision_score, pos_label = 1))
 		clf.fit(fT, lT)
 		print("-----\nBest parameters set found for Logistic Regression tuning precision:\n-----")
 		print(clf.best_params_)
 		print(classification_report(lt, clf.predict(ft)))
 
+	def kek(self, proba, proba_tol):
+		file, exp_file = 'corpus_scores\\v2_5_raw_inv.txt', 'corpus_scores\\10_opt_raw.txt'
+		data = dr(file).read_data(precision = self.precision)
+		features = data[:, 2:-1]
+		labels = data[:, -1].astype(int)
+		exp_data = dr(exp_file).read_data(precision = self.precision)
+		exp_features = data[:, 2:-1]
+		exp_labels = data[:, -1].astype(int)
+		
+		f1, f2, l1, l2 = train_test_split(features, labels, test_size = 0.5, random_state = 0)
+		ef1, ef2, el1, el2 = train_test_split(exp_features, exp_labels, test_size = 0.5, random_state = 0)
+		self.train(ef1, el1)
+		if proba:
+			preds = self.proba_util(f2, proba_tol)
+			result = [precision_score(l2, preds, pos_label = 1)]
+			dr.print_report(self.debug, preds, l2) #Print Classification Report
+			
+			self.train(ef2, el2)
+			preds = self.proba_util(f1, proba_tol)
+			result.append(precision_score(l1, preds, pos_label = 1))
+			dr.print_report(self.debug, preds, l1) #Print Classification Report
+		else:
+			preds = self.regr.predict(f2)
+			result = [precision_score(l2, preds, pos_label = 1)]
+			dr.print_report(self.debug, preds, l2) #Print Classification Report
+			
+			self.train(ef2, el2)
+			preds = self.regr.predict(f1)
+			result.append(precision_score(l1, preds, pos_label = 1))
+			dr.print_report(self.debug, preds, l1) #Print Classification Report
+		return result
+
+	def proba_util(self, features, proba_tol):
+		preds = []
+		feature = [features[0]]
+		for i, x in enumerate(features[1:]):
+			if features[i][-2] > x[-2]:
+				preds.extend(dr.predict_from_proba(self.regr.predict_proba(feature), proba_tol))
+				feature = [x]
+			else:
+				feature.append(x)
+		preds.extend(dr.predict_from_proba(self.regr.predict_proba(feature), proba_tol))
+		return preds
+
 def main(argv):
-	debug, proba, tune, optimise, exp = False, False, False, False, False
+	debug, proba, tune, optimize, exp, final = False, False, False, False, False, False
+	proba_tol = 0.5 #0.7 precision; 0.5 f1_score
 	file = 'corpus_scores\\v2_5_raw_inv.txt'
 	try:
-		opts, args = getopt.getopt(argv,"dpeto")
+		opts, args = getopt.getopt(argv,"dpetof")
 	except getopt.GetoptError:
 		sys.exit(2)
 	for opt, arg in opts:
@@ -126,16 +143,21 @@ def main(argv):
 		elif opt == '-t':
 			tune = True
 		elif opt == '-o':
-			optimise = True
+			optimize = True
+		elif opt == '-f':
+			final = True
 
-	my_trainer = Regression(-1, debug = debug, optimise = optimise, exp = exp)
-	if tune:
-		my_trainer.tune_parameters(file)
+	my_trainer = Regression(-1, debug = debug, optimize = optimize, exp = exp)
+	if final:
+		print(my_trainer.kek(proba, proba_tol))
 	else:
-		if proba:
-			print(my_trainer.validate_proba(file, 0.872))
+		if tune:
+			my_trainer.tune_parameters(file)
 		else:
-			print(my_trainer.validate(file))
+			if proba:
+				print(my_trainer.validate_proba(file, proba_tol))
+			else:
+				print(my_trainer.validate(file))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- mlld/svm_trainer.py
+++ mlld/svm_trainer.py
@@ -1,21 +1,24 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import KFold, train_test_split, GridSearchCV
-from sklearn.metrics import classification_report, precision_score, make_scorer
+from sklearn.metrics import classification_report, precision_score, f1_score, make_scorer
 from utils.data_reader import DataReader as dr
 import sys, getopt, numpy as np
 
 class SVMTrainer(object):
 
-	def __init__(self, decimal_precision, debug, optimise, exp):
+	def __init__(self, decimal_precision, debug, optimize, exp):
 		np.set_printoptions(threshold = np.inf)
 		self.precision = decimal_precision
 		self.debug = debug
-		self.svc = SVC(class_weight = 'balanced')
-		if optimise:
+		self.svc = SVC(class_weight = 'balanced', probability = True)
+		if optimize:
 			if exp:
-				self.svc = SVC(class_weight = 'balanced', C = 10, gamma = 1e-3, kernel = 'rbf', shrinking = True)#experimental optimised
+				self.svc = SVC(class_weight = 'balanced', C = 10, gamma = 1e-3, kernel = 'rbf', shrinking = True)#experimental optimized
 			else:
-				self.svc = SVC(class_weight = 'balanced', C = 1000, gamma = 1e-3, kernel = 'rbf', shrinking = True)#, probability = True)
+				# precision and f1_score optimized
+				self.svc = SVC(class_weight = 'balanced', C = 1000, gamma = 1e-3, kernel = 'rbf', shrinking = True, probability = True)
+				# f1_score optimized
+				self.svc = SVC(class_weight = 'balanced', C = 10, gamma = 1e-3, kernel = 'rbf', shrinking = True, probability = True)
 
 	def scale_data(self, train_data):
 		self.scaler = preprocessing.StandardScaler().fit(train_data)
@@ -33,17 +36,12 @@ class SVMTrainer(object):
 		labels = data[:, -1].astype(int)
 		
 		print('[INFO] ', np.count_nonzero(labels != 0), 'links present in training data, out of ', len(labels))
-
 		k_fold = KFold(n_splits = 2)
 		for train, test in k_fold.split(features):
-			print('[INFO] Round of testing')
 			self.train(features[train], labels[train])
-			
 			preds = self.svc.predict(features[test])
 			print('[INFO] ', np.count_nonzero(preds != 0), ' links retrieved by model')
-			
-			succ_pred = np.count_nonzero(preds == labels[test])
-			print('[INFO] Succesfully predicted ', succ_pred, ' links out of ', len(labels[test]))
+			print('[INFO] Succesfully predicted ', np.count_nonzero(preds == labels[test]), ' links out of ', len(labels[test]))
 
 	def validate(self, file):
 		data = dr(file).read_data(precision = self.precision)
@@ -55,11 +53,8 @@ class SVMTrainer(object):
 		for train, test in k_fold.split(features):
 			self.train(features[train], labels[train])
 			preds = self.svc.predict(features[test])
-			precision = precision_score(labels[test], preds, pos_label = 1, average = 'binary')
-			if self.debug:
-				print('[INFO] ', np.count_nonzero(preds != 0), ' links retrieved by model')
-				print(classification_report(labels[test], preds, target_names = ['class 0', 'class 1']))
-			result.append(precision)
+			result.append(precision_score(labels[test], preds, pos_label = 1, average = 'binary'))
+			dr.print_report(self.debug, preds, labels[test]) #Print Classification Report
 		return result
 
 	def validate_proba(self, file, proba_tol):
@@ -71,31 +66,9 @@ class SVMTrainer(object):
 		k_fold = KFold(n_splits = 2)
 		for train, test in k_fold.split(features):
 			self.train(features[train], labels[train])
-			preds = []
-			feature = [features[test[0]]]
-			for x in test[1:]:
-				if features[x - 1][-2] > features[x][-2]:
-					preds.extend(self.predict_from_proba(self.svc.predict_proba(feature), proba_tol))
-					feature = [features[x]]
-				else:
-					feature.append(features[x])
-			preds.extend(self.predict_from_proba(self.svc.predict_proba(feature), proba_tol))
-			if self.debug:
-				print('[INFO] ', np.count_nonzero(preds != 0), ' links retrieved by model')
-				print(classification_report(labels[test], preds, target_names = ['class 0', 'class 1']))
-			result.append(precision_score(labels[test], preds, pos_label = 1, average = 'binary'))
-		return result
-
-	def predict_from_proba(self, proba, proba_tol):
-		max = 0
-		for i, val in enumerate(proba):
-			if val[1] > max and val[1] >= proba_tol:
-				max = val[1]
-				idx = i
-
-		result = np.zeros(len(proba), dtype = int)
-		if max != 0:
-			result[idx] = 1
+			preds = self.proba_util(features[test], proba_tol)
+			result.append(precision_score(labels[test], preds, pos_label = 1))
+			dr.print_report(self.debug, preds, labels[test]) #Print Classification Report
 		return result
 
 	def tune_parameters(self, file):
@@ -104,17 +77,31 @@ class SVMTrainer(object):
 		labels = data[:, -1].astype(int)
 		
 		fT, ft, lT, lt = train_test_split(features, labels, test_size = 0.5, random_state = 0)
-		parameters = [{'C': [1, 10, 100, 1000], 'gamma': [1e-3, 1e-4], 'kernel': ['linear', 'poly', 'rbf'], 'shrinking': [True, False]}]
+		parameters = [{'C': [10, 100, 500, 1000], 'gamma': [1e-3, 1e-4, 1e-5], 'kernel': ['rbf', 'poly'], 
+			'shrinking': [True, False], 'class_weight': ['balanced']}]
 		
-		clf = GridSearchCV(SVC(class_weight = 'balanced'), parameters, cv = 3, scoring = make_scorer(precision_score, pos_label = 1))
+		clf = GridSearchCV(SVC(), parameters, cv = 2, scoring = make_scorer(f1_score, pos_label = 1))
 		clf.fit(fT, lT)
-		print("-----\nBest parameters set found for SVM tuning for precision:\n-----")
+		print("-----\nBest parameters set found for SVM tuning precision:\n-----")
 		print(clf.best_params_)
 		print(classification_report(lt, clf.predict(ft)))
 
+	def proba_util(self, features, proba_tol):
+		preds = []
+		feature = [features[0]]
+		for i, x in enumerate(features[1:]):
+			if features[i][-2] > x[-2]:
+				preds.extend(dr.predict_from_proba(self.svc.predict_proba(feature), proba_tol))
+				feature = [x]
+			else:
+				feature.append(x)
+		preds.extend(dr.predict_from_proba(self.svc.predict_proba(feature), proba_tol))
+		return preds
+
 def main(argv):
-	debug, proba, tune, optimise, exp = False, False, False, False, False
+	debug, proba, tune, optimize, exp = False, False, False, False, False
 	file = 'corpus_scores\\v2_5_raw_inv.txt'
+	proba_tol = 0.28 #0.28 precision; 0.25 f1_score
 	try:
 		opts, args = getopt.getopt(argv,"dpeto")
 	except getopt.GetoptError:
@@ -130,14 +117,14 @@ def main(argv):
 		elif opt == '-t':
 			tune = True
 		elif opt == '-o':
-			optimise = True
+			optimize = True
 
-	my_trainer = SVMTrainer(-1, debug = debug, optimise = optimise, exp = exp)
+	my_trainer = SVMTrainer(-1, debug = debug, optimize = optimize, exp = exp)
 	if tune:
 		my_trainer.tune_parameters(file)
 	else:
 		if proba:
-			print(my_trainer.validate_proba(file, 0.0))
+			print(my_trainer.validate_proba(file, proba_tol))
 		else:
 			print(my_trainer.validate(file))
 
